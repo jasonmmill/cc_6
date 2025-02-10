d840cb6ea6acf57666aa7fc39e98d47b0f54251f
Jasonmmill <jasonmatthewmill@gmail.com>
2025-02-09 21:39:31 -0500

    Task 2 - Sales Tax Computation

--- cc_6.js
+++ cc_6.js
@@ -1,5 +1,12 @@
 // Task 1: Function Declaration
-function calculateProfit(costPrice, sellingPrice, unitsSold) {
+function calculateProfit(costPrice, sellingPrice, unitsSold) { // create function and add parameters
     return (sellingPrice - costPrice) * unitsSold
 }
-console.log(`Total Profit: $${calculateProfit(20,30,100)}`)
+console.log(`Total Profit: $${calculateProfit(20,30,100)}`) // log results using template literal
+
+// Task 2: Function Expression
+function calculateSalesTax(amount, taxRate) { // create function and add parameters
+    return amount * taxRate
+}
+console.log(`Sales tax: $${calculateSalesTax(100,0.1)}`) // log results using template literal
+
